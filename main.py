b551146afcd05985988cc6adde0615807ee3f24d
AlexandrBolvachev <sasabolvacev@gmail.com>
2025-02-24 23:47:47 +0300

    картинка

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 def main(title):
     return render_template('content.html', title=title)
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('content.html', prof=prof.lower())
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
